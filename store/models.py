fe7577d68f86997b21c7af25cc24b57fdf402588
Laim-M <thelaimm@gmail.com>
2019-11-01 16:18:33 +0300

    ready store

--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+import PIL
 from PIL import Image
 
 class UserProfileInfo(models.Model):
